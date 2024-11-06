e14c10b3e714ef7722df66669477b76b5468cfbe
Cha Hun Lee <32297705+ASGDLO@users.noreply.github.com>
2024-11-06 15:09:57 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { redirect } from 'next/navigation';
 import React from 'react';
 import Link from 'next/link';
 
 export default function HomePage() {
+  // Redirect to the /ko route
+  redirect('/ko');
+
+  // The below JSX will not be rendered because of the redirect
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1>Select Language</h1>
